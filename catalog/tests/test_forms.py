8830ec58c5e0241268c622f409178b27c33596f2
Vladyslav Hnatchenko <vladyslav.hnatchenko@eleks.com>
2018-12-03 11:21:24 +0200

    Fix some tests

--- catalog/tests/test_forms.py
+++ catalog/tests/test_forms.py
@@ -38,4 +38,4 @@ class RenewBookFormTest(TestCase):
     def test_renew_form_date_field_help_text(self):
         """TEst renewal_date help_text is as expected."""
         form = RenewBookForm()
-        self.assertEqual(form.fields['renewal_date'].help_text, "Enter a date beetwen now and 4 weeks (default 3).")
+        self.assertEqual(form.fields['renewal_date'].help_text, 'Enter a date between now and 4 weeks (default 3).')
